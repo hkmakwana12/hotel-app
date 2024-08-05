93b1df2ca8a3ea6461f265d9ffc85fd487b2baf5
Hitesh Makwana <hkmakwana1212@gmail.com>
2024-08-05 14:44:48 +0530

    Update HomePage.jsx

--- src/pages/frontend/HomePage.jsx
+++ src/pages/frontend/HomePage.jsx
@@ -1,5 +1,4 @@
 import { Helmet } from "react-helmet-async";
-import SearchPropertyBill from "src/modules/property-tax/sections/property/search-property-bill";
 
 export default function HomePage() {
   return (
@@ -7,8 +6,6 @@ export default function HomePage() {
       <Helmet>
         <title>Home Page | {import.meta.env.VITE_APP_NAME}</title>
       </Helmet>
-
-      <SearchPropertyBill />
     </>
   );
 }
